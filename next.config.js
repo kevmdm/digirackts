e610052b07ccabb3f275f9b4215fa60e89bb40da
slipiduche <alejandrocamacaro91@gmail.com>
2022-03-31 10:56:53 -0400

    adding cardano files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Important: return the modified config
+    config.experiments.asyncWebAssembly = true;
+    config.module.exprContextCritical = false;
+    return config;
+  },
+  images: {
+    domains: ['ipfs.io', 'ipfs.blockfrost.dev'],
+  },
 }
 
 module.exports = nextConfig
