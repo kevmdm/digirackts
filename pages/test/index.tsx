a92d38078994af3b500501bf47e18eace180c851
Kevin <kevmdm@gmail.com>
2022-04-07 17:54:06 -0400

    Footer creado

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -5,9 +5,12 @@ import styles from "../styles/Home.module.css";
 import Dapp from "../../cardano/TestingApp";
 import { Layout } from "../../components/layouts";
 import { Box, Typography, Link, IconButton, Button, Grid } from "@mui/material";
-import { styled } from '@mui/material/styles';
+import { styled } from "@mui/material/styles";
+import NextLink from "next/link";
 
-import { flexbox } from "@mui/system";
+import { bgcolor, flexbox } from "@mui/system";
+import { red } from "@mui/material/colors";
+import { Padding } from "@mui/icons-material";
 
 export default function TestPage() {
   return (
@@ -20,7 +23,9 @@ export default function TestPage() {
           backgroundColor: "#F9F9F9",
         }}
       >
-        <Grid container sx={{ ml: "77px", paddingTop: "4%" }}>
+        <Grid container sx={{ ml: "4%", paddingTop: "4%" }}>
+          {/* Logo row start  */}
+
           <Grid item xs={6}>
             <Grid container>
               <Grid item xs={12}>
@@ -31,9 +36,11 @@ export default function TestPage() {
                     aria-label="account of current user"
                     aria-haspopup="true"
                     color="inherit"
-                    sx={{'&:hover': {
-                      backgroundColor: 'transparent',
-                    },}}
+                    sx={{
+                      "&:hover": {
+                        backgroundColor: "transparent",
+                      },
+                    }}
                   >
                     <Image
                       src={"/img/navbar/textLogo.png"}
@@ -45,27 +52,114 @@ export default function TestPage() {
               </Grid>
 
               <Grid item xs={12}>
-                <Typography paddingRight={"30%"} paddingTop={"4%"}>
+                <Typography
+                  paddingRight={"30%"}
+                  paddingTop={"4%"}
+                  paddingLeft={"2%"}
+                >
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   Necessitatibus vitae earum provident iste enim quia officiis
                   temporibus, quasi adipisci alias, dolorem ratione laboriosam
                   rem maxime? Earum, veniam distinctio? Quibusdam, dolore?
                 </Typography>
               </Grid>
+
+              <Box sx={{ gridRowStart: "auto" }}>
+                <NextLink href={"/"}>
+                  <IconButton
+                    size="large"
+                    edge="end"
+                    aria-label="account of current user"
+                    aria-haspopup="true"
+                    color="inherit"
+                    sx={{
+                      "&:hover": {
+                        backgroundColor: "transparent",
+                      },
+                    }}
+                  >
+                    <Image
+                      src={"/img/footerLogos/twitter.png"}
+                      width="90px"
+                      height={"90px"}
+                    ></Image>
+                  </IconButton>
+                </NextLink>
+                <NextLink href={"/"}>
+                  <IconButton
+                    size="large"
+                    edge="end"
+                    aria-label="account of current user"
+                    aria-haspopup="true"
+                    color="inherit"
+                    sx={{
+                      "&:hover": {
+                        backgroundColor: "transparent",
+                      },
+                    }}
+                  >
+                    <Image
+                      src="/img/footerLogos/discord.png"
+                      width="90px"
+                      height={"90px"}
+                    ></Image>
+                  </IconButton>
+                </NextLink>
+              </Box>
             </Grid>
           </Grid>
           <Grid item xs={2} sx={{ paddingTop: "30px" }}>
-            {" "}
-            <Typography variant="h6">Browse</Typography>{" "}
+            {/* browse start  */}
+
+            <Grid container>
+              <Grid item xs={12}>
+                <Typography variant="h6">Browse</Typography>
+              </Grid>
+
+              <Grid item xs={12} sx={{ paddingTop: "10%" }}>
+                <NextLink href="/explore" passHref>
+                  <Link variant="h6" underline="none" sx={{fontWeight:"fontWeightLight", color:"#131313"}}>
+                    Market Place
+                  </Link>
+                </NextLink>
+              </Grid>
+            </Grid>
           </Grid>
+
+          {/* creators start  */}
+
           <Grid item xs={2} sx={{ paddingTop: "30px" }}>
-            <Typography variant="h6">Creators</Typography>{" "}
+            <Grid container>
+              
+              <Grid item xs={12}>
+                <Typography variant="h6">Creators</Typography>{" "}
+              </Grid>
+
+              <Grid item xs={12} sx={{ paddingTop: "10%" }}>
+                <NextLink href="/LaunchPad" passHref>
+                  <Link variant="h6" underline="none" sx={{fontWeight:"fontWeightLight", color:"#131313"}}>
+                    Launch Pad
+                  </Link>
+                </NextLink>
+              </Grid>
+
+              <Grid item xs={12} sx={{ paddingTop: "10%" }}>
+                <NextLink href="/verify-your-collection" passHref>
+                  <Link variant="h6" underline="none" sx={{fontWeight:"fontWeightLight", color:"#131313"}}>
+                    Verify Your Collection
+                  </Link>
+                </NextLink>
+              </Grid>
+
+
+            </Grid>
           </Grid>
-          <Grid item xs={2} sx={{ paddingTop: "30px" }}>
-            {" "}
-            <Link>
-              <Typography variant="h6">About us</Typography>
-            </Link>
+
+          <Grid item xs={2}  sx={{ paddingTop: "30px" }}>
+            {/* About us start */}
+            <NextLink href="/about-us">
+              <Link variant="h6">About us</Link>
+            </NextLink>
           </Grid>
         </Grid>
       </Box>
